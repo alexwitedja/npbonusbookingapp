3b264c26c8f1cbc7c1fa364c3a7b20a4d10ffe7d
alexwitedja <s3641938@student.rmit.edu.au>
2020-05-08 13:57:57 +1000

    added generateTimeslots

--- Timeslot.js
+++ Timeslot.js
@@ -1,6 +1,21 @@
 const timeslots = [
-    "8:30",
-    "8:40"
 ]
+
+generateTimeslots(17)
+
+function generateTimeslots(hours) {
+    // Creates 10 minute interval timeslots from 8 to X hours.
+    for(var i = 8; i < hours; i++) {
+        for(var j = 0; j < 6 + 1; j++) {
+            var timeslot = 00;
+            if (j == 6) {
+                timeslot = i + 1 + ":00"
+            } else {
+                timeslot = i + ":" + j + "0"
+            }
+            timeslots.push(timeslot);
+        }
+    }
+}
 
 module.exports = timeslots;
